e3f16b9907055b4e93c758dcdb6816f97e144569
andsel <selva.andre@gmail.com>
2016-04-02 13:29:55 +0200

    Moved quality upperbound check (>50) at the end of the loop

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,21 +20,15 @@ class GildedRose {
                     }
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes")) {
-                        if (item.sellIn < 10) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                if (item.name.equals("Backstage passes")) {
+                    if (item.sellIn < 10) {
+                        item.quality = item.quality + 1;
+                    }
 
-                        if (item.sellIn < 5) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                    if (item.sellIn < 5) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
@@ -48,12 +42,10 @@ class GildedRose {
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = item.quality + 1;
                 }
             }
 
