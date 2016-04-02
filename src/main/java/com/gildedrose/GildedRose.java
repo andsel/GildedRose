b24b28a7a3358823b1c912ff098e26587055670a
andsel <selva.andre@gmail.com>
2016-04-02 13:24:22 +0200

    Moved upfront and extracted into a method the decrease of days

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,61 +8,64 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            decrementDays(item);
+            if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras")) {
+                        item.quality = item.quality - 1;
                     } else {
-                        items[i].quality = 80;
+                        item.quality = 80;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals("Backstage passes")) {
+                        if (item.sellIn < 10) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 5) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
 
-            if (items[i].quality > 50 && !items[i].name.equals("Sulfuras")) {
-                items[i].quality = 50;
+            if (item.quality > 50 && !item.name.equals("Sulfuras")) {
+                item.quality = 50;
             }
+        }
+    }
+
+    private void decrementDays(Item item) {
+        if (!item.name.equals("Sulfuras")) {
+            item.sellIn = item.sellIn - 1;
         }
     }
 }
