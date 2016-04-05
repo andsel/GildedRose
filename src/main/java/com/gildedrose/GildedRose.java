f18770057c166821590d9c8b9af8fddf0f7948ce
andsel <selva.andre@gmail.com>
2016-04-05 21:54:43 +0200

    Grouped common parts of behaviour

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,54 +10,59 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             decrementDays(item);
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras")) {
-                        item.quality = item.quality - 1;
-                    } else {
-                        item.quality = 80;
-                    }
-                }
-            } else {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals("Backstage passes")) {
-                    if (item.sellIn < 10) {
-                        item.quality = item.quality + 1;
-                    }
-
-                    if (item.sellIn < 5) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+            if (item.name.equals("Sulfuras")) {
+                item.quality = 80;
+                break;
             }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
+            if (item.name.equals("Backstage passes")) {
+                if (item.sellIn < 10) {
+                    item.quality = item.quality + 1;
+                }
+
+                if (item.sellIn < 5) {
                     item.quality = item.quality + 1;
                 }
             }
 
-            if (item.quality > 50 && !item.name.equals("Sulfuras")) {
+            if (isOlderGetBetter(item)) {
+                item.quality = item.quality + 1;
+            } else {
+                decreaseQuality(item);
+            }
+
+            if (item.sellIn < 0) {
+                if (item.name.equals("Backstage passes")) {
+                    item.quality = -1;
+                }
+
+                if (isOlderGetBetter(item)) {
+                    item.quality = item.quality + 1;
+                } else {
+                    decreaseQuality(item);
+                }
+            }
+
+            if (item.quality > 50) {
                 item.quality = 50;
             }
         }
     }
 
-    private void decrementDays(Item item) {
-        if (!item.name.equals("Sulfuras")) {
-            item.sellIn = item.sellIn - 1;
+    private boolean isOlderGetBetter(Item item) {
+        return item.name.equals("Aged Brie") || item.name.equals("Backstage passes");
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
+    }
+
+    private void decrementDays(Item item) {
+        if (item.name.equals("Sulfuras")) {
+            return;
+        }
+        item.sellIn = item.sellIn - 1;
     }
 }
